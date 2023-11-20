4ba97941b3e14d59c2a84fe98a86442febedb3bb
default-none <foxrain426@gmail.com>
2023-11-20 09:51:46 +0900

    main 0.0.3

--- src/components/tabletModel/TabletModel.js
+++ src/components/tabletModel/TabletModel.js
@@ -7,17 +7,20 @@ const TabletModel = () => {
   const [wheelDirect, setWheelDirect] = useState(false);
   const modelRef = useRef();
 
-  function handleRotate(e){
-    if(e.deltaY <0){
-      modelRef.current.rotation
+  function handleRotate(e) {
+    if (e.deltaY < 0) {
+      modelRef.current.rotation.y += 0.3;
+      modelRef.current.position.x += 0.3;
+      console.log(modelRef.current.rotation.x);
     }
   }
 
-
-
   return (
     <>
-      <Canvas camera={{ position: [0, 0, 11], fov: 60, near: 0.1, far: 1000 }} onWheel={handleRotate}>
+      <Canvas
+        camera={{ position: [0, 0, 11], fov: 60, near: 0.1, far: 1000 }}
+        onWheel={handleRotate}
+      >
         <Lights />
         <Model modelRef={modelRef} />
       </Canvas>
