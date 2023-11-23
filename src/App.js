2c097e9482d88bf5e65c2a166fcd13a730036047
jeonwanjin <jwj1212121@gmail.com>
2023-11-23 15:49:49 +0900

    스터디페이지 완성(링크제외)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Page as MainPage } from "./pages/MainPage/Page";
 import Login from "./pages/Login/KakaoLogin";
+import StudyPage from "./pages/StudyPage/Studypage";
+import StudyBook from "./pages/StudyBook/StudyBook";
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <MainPage />
-            </>
-          }
-        />
+        <Route path="/" element={<><MainPage /></>}/>
+        <Route path="/StudyPage" element={<StudyPage/>}/>
+        <Route path="/StudyBook" element={<StudyBook/>}/>
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
