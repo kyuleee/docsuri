0e2a1dd543a35909f8d25649b6f4d93f70d44df0
kyuleee <mindy7453@naver.com>
2023-11-23 17:29:21 +0900

    1123

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Page as MainPage } from "./pages/MainPage/Page";
 import Login from "./pages/Login/KakaoLogin";
+import Join from "./pages/Login/Join";
+import MyPage from "./pages/MyPage/MyPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <MainPage />
-            </>
-          }
-        />
+        <Route path="/" element={<><MainPage /></>}/>
         <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </>
   );
