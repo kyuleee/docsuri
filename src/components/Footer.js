2526283a1ea63e02d2b5bdf46cdd6474ef7f1e2f
minkoo9504 <mk9504@naver.com>
2023-11-24 14:36:48 +0900

    sibal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,21 @@ import "./Footer.css";
 const Footer = () => {
     return ( 
         <div className="Footer">
-            <div className="footerIcon">
-                <div className="footerLogo">
-                    <div className="FLogo"></div>
-                    <div className="footerTxt">
-                        <p>메인</p>
-                        <p>|</p>
-                        <p>교과 학습</p>
-                        <p>|</p>
-                        <p>문제 풀이</p>
-                        <p>|</p>
-                        <p>마이페이지</p>
-                        <p>|</p>
-                        <p>자주하는 질문</p>
+            <div>
+                <div className="footerIcon">
+                    <div className="footerLogo">
+                        <div className="FLogo"></div>
+                    </div>
+                    <div className="iconLink">
+                        <div className="Ficon"></div>
+                        <div className="Yicon"></div>
+                        <div className="Iicon"></div>
+                        <div className="Kicon"></div>
                     </div>
                 </div>
-                <div>
-                    <div className="Ficon"></div>
-                    <div className="Yicon"></div>
-                    <div className="Iicon"></div>
-                    <div className="Kicon"></div>
+                <div className="adress">
+                    Copyright ⓒ 2023 독수리 어쩌고 All rights reserved.
                 </div>
-            </div>
-            <div className="adress">
-                Copyright ⓒ 2023 독수리 어쩌고 All rights reserved.
             </div>
         </div>
      );
--- src/pages/SideBar/SideBar.js
+++ src/pages/SideBar/SideBar.js
@@ -9,24 +9,28 @@ const SideBar = ()=>{
             <nav>
                 <ul>
                     <li>
-                        <div className='icon'>
+                        <div className='icon icon1'>
                             <img src={nav1}/>
                         </div>
                         <p>교과 학습</p>
                     </li>
                     <li>
-                        <div className='icon'>
+                        <div className='icon icon2'>
                             <img src={nav2}/>
                         </div>
                         <p>챌린지</p>
                     </li>
-                    <li>
-                        <div className='icon'>MY</div>
+                    {/* <li>
+                        <div className='icon icon3'><p className="my">MY</p></div>
                         <p>마이페이지</p>
+                    </li> */}
+                    <li>
+                        <div className='icon icon4'><p className="faq">FAQ</p></div>
+                        <p>자주하는 질문</p>
                     </li>
                     <li>
-                        <div className='icon'>FAQ</div>
-                        <p>자주하는 질문</p>
+                        <div className='icon icon5'><p className="su">수</p></div>
+                        <p>수다학</p>
                     </li>
                 </ul>
             </nav>
