9bcd3f488de781a867234c9f36f272b9734312db
default-none <foxrain426@gmail.com>
2023-11-24 11:08:18 +0900

    main 0.0.7

--- src/pages/MainPage/section/ChallengeSection.js
+++ src/pages/MainPage/section/ChallengeSection.js
@@ -1,17 +1,34 @@
+import { Link } from "react-router-dom";
 import ChallengeBox from "../../../components/contentBox/ChallengeBox";
 import MenuTitleText from "../../../components/text/MenuTitleText";
 import "./ChallengeSection.css";
-const ChallengeSection = ({ ChallengeSectionRef }) => {
+import Arrow from "./datas/svg/Arrow";
+import { useState } from "react";
+const ChallengeSection = ({ ChallengeSectionRef, challengeON }) => {
+  const [resize, setResize] = useState(false);
+  function handleArrow() {}
   return (
     <section className="ChallengeSection" ref={ChallengeSectionRef}>
-      <div className="ChallengeSpace">
-        <div>
+      <div
+        className="ChallengeSpace"
+        style={challengeON ? { opacity: 1 } : { opacity: 0 }}
+      >
+        <div className="titleWrap">
           <MenuTitleText
             text={"챌린지"}
             text2={"CHALLENGE"}
             text3={"챌린지로 실전처럼 준비하고,"}
             text4={"수학 열공 챌린지로 공부 습관 들이고 스탬프 받자!"}
           />
+          <div
+            className="more"
+            onMouseEnter={handleArrow}
+            onMouseLeave={handleArrow}
+          >
+            <Link>
+              더보기 <Arrow />
+            </Link>
+          </div>
         </div>
         <ChallengeBox text={"규칙찾기 수학 챌린지"} />
         <ChallengeBox text={"수학 블랙 챌린지"} />
