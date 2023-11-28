62fa61759674a32e7d2697ee27db1312ab022482
default-none <144871416+default-none@users.noreply.github.com>
2023-11-28 17:50:45 +0900

    test last maybe

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,16 +1,17 @@
-import users from "../../datas/userData.json"
+import { useRef } from "react";
+import users from "../../datas/userData.json";
 const MyPage = () => {
-    const userData = {...sessionStorage};
-    const dataview = ()=>{
-        console.log(sessionStorage)
-        console.log(userData)
-    }
+  const userRef = useRef({ ...sessionStorage });
+  const dataview = () => {
+    console.log(sessionStorage);
+    console.log(userRef.current);
+  };
 
-    return ( 
-        <>
-            <button onClick={dataview}>item</button>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <button onClick={dataview}>1234</button>
+    </>
+  );
+};
+
 export default MyPage;
